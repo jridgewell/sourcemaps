9a0266518d2877047da27cbaf3290a4e6e3cb556
Justin Ridgewell <justin@ridgewell.name>
2025-06-29 15:54:37 -0400

    Reorder exports, generate type maps

--- mts-types.mjs
+++ mts-types.mjs
@@ -3,21 +3,21 @@ import { readFileSync, writeFileSync, readdirSync, unlinkSync } from 'node:fs';
 const packagePath = process.cwd();
 const typesDir = `${packagePath}/types`;
 
-function rename(path, ext) {
-  return path.replace(/\.ts(\.map)?$/, `.${ext}$1`);
-}
-
 const from = /\bfrom (['"])(\.[^'"]*)\1;/g;
+const tsExt = /\.ts(\.map)?$/;
+
 for (const file of readdirSync(typesDir)) {
   if (!file.endsWith('.d.ts') && !file.endsWith('.d.ts.map')) continue;
 
   const path = `${typesDir}/${file}`;
   const contents = readFileSync(path, 'utf8');
 
-  const cts = contents.replace(/^export default/gm, 'export =').replace(from, 'from $1$2.cts$1;');
+  const cts = contents
+    .replace(/^export default/gm, 'export =      ')
+    .replace(from, 'from $1$2.cts$1;');
   const mts = contents.replace(from, 'from $1$2.mts$1;');
 
   unlinkSync(path);
-  writeFileSync(rename(path, 'cts'), cts);
-  writeFileSync(rename(path, 'mts'), mts);
+  writeFileSync(path.replace(tsExt, `.cts$1`), cts);
+  writeFileSync(path.replace(tsExt, `.mts$1`), mts);
 }
