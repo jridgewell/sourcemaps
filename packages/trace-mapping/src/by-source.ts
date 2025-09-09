5739a6f860131821245fdf2617a4522959a270aa
Connor Peet <connor@peet.io>
2025-09-09 11:30:26 -0700

    fix: pathological slow case in bySource
    Hacked together a bit to fix #41. The use of a proxy here is fairly
    cursed but I wanted to validate if this approach would fix it, which
    it does -- tests pass and the 24s case goes is 90ms on my machine.
    
    Feel free to rewrite or suggest a cleaner approach :stuck_out_tongue:

--- packages/trace-mapping/src/by-source.ts
+++ packages/trace-mapping/src/by-source.ts
@@ -16,6 +16,7 @@ export default function buildBySources(
   memos: MemoState[],
 ): Source[] {
   const sources: Source[] = memos.map(buildNullArray);
+  const todo: ReverseSegment[][][] = [];
 
   for (let i = 0; i < decoded.length; i++) {
     const line = decoded[i];
@@ -26,33 +27,63 @@ export default function buildBySources(
       const sourceIndex = seg[SOURCES_INDEX];
       const sourceLine = seg[SOURCE_LINE];
       const sourceColumn = seg[SOURCE_COLUMN];
-      const originalSource = sources[sourceIndex];
+
+      todo[sourceIndex] ||=[];
+      (todo[sourceIndex][sourceLine] ||= []).push([sourceColumn, i, seg[COLUMN]]);
+    }
+  }
+
+  for (let sourceIndex = 0; sourceIndex < todo.length; sourceIndex++) {
+    const lines = todo[sourceIndex];
+    if (!lines) continue;
+    const originalSource = sources[sourceIndex];
+    const memo = memos[sourceIndex];
+
+    for (let sourceLine = 0; sourceLine < lines.length; sourceLine++) {
+      const segs = lines[sourceLine];
+      if (!segs) continue;
+
+      segs.sort((a, b) => a[0] - b[0]);
       const originalLine = (originalSource[sourceLine] ||= []);
-      const memo = memos[sourceIndex];
 
-      // The binary search either found a match, or it found the left-index just before where the
-      // segment should go. Either way, we want to insert after that. And there may be multiple
-      // generated segments associated with an original location, so there may need to move several
-      // indexes before we find where we need to insert.
-      let index = upperBound(
-        originalLine,
-        sourceColumn,
-        memoizedBinarySearch(originalLine, sourceColumn, memo, sourceLine),
-      );
+      let lastIndex = 0;
+      const newOriginalLine: ReverseSegment[] = [];
+      for (let i = 0; i < segs.length; i++) {
+        const seg = segs[i];
 
-      memo.lastIndex = ++index;
-      insert(originalLine, index, [sourceColumn, i, seg[COLUMN]]);
+        // The binary search either found a match, or it found the left-index just before where the
+        // segment should go. Either way, we want to insert after that. And there may be multiple
+        // generated segments associated with an original location, so there may need to move several
+        // indexes before we find where we need to insert.
+        const originalLineJoint = jointArray(newOriginalLine, originalLine, lastIndex);
+        let index = upperBound(
+          originalLineJoint,
+          seg[0],
+          memoizedBinarySearch(originalLineJoint, seg[0], memo, sourceLine),
+        );
+
+        memo.lastIndex = ++index;
+        if (index <= newOriginalLine.length) {
+          insert(newOriginalLine, index, seg);
+          continue;
+        }
+
+        index = index - newOriginalLine.length + lastIndex;
+        while (lastIndex < index) {
+          newOriginalLine.push(originalLine[lastIndex++]);
+        }
+
+        newOriginalLine.push(seg);
+      }
+
+      while (lastIndex < originalLine.length) {
+        newOriginalLine.push(originalLine[lastIndex++]);
+      }
+      originalSource[sourceLine] = newOriginalLine;
     }
   }
 
   return sources;
-}
-
-function insert<T>(array: T[], index: number, value: T) {
-  for (let i = array.length; i > index; i--) {
-    array[i] = array[i - 1];
-  }
-  array[index] = value;
 }
 
 // Null arrays allow us to use ordered index keys without actually allocating contiguous memory like
@@ -62,4 +93,22 @@ function insert<T>(array: T[], index: number, value: T) {
 // order when iterating with for-in.
 function buildNullArray<T extends { __proto__: null }>(): T {
   return { __proto__: null } as T;
+}
+
+function jointArray<T>(a: T[], b: T[], bOffset: number): T[] {
+  return new Proxy([], {
+    get(_, p) {
+      if (p === 'length') return a.length + (b.length - bOffset);
+      const index = typeof p === 'symbol' ? NaN : parseInt(p, 10);
+      if (isNaN(index)) return (a as any)[p];
+      return index < a.length ? a[index] : b[index - bOffset];
+    }
+  })
+}
+
+function insert<T>(array: T[], index: number, value: T) {
+  for (let i = array.length; i > index; i--) {
+    array[i] = array[i - 1];
+  }
+  array[index] = value;
 }
